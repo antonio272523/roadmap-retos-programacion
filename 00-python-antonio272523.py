665676828e202caddef549677dd43ea250ef3b71
antonio272523 <155701035+antonio272523@users.noreply.github.com>
2024-01-06 16:33:24 -0600

    Add files via upload

---
+++ 00-python-antonio272523.py
@@ -0,0 +1,32 @@
+'''reto 00'''
+'''instruccion 1: crea un comentario en el codigo y coloca la url del sitio web oficial del lenguaje de programacion que has seleccionado'''
+#https://python.org
+
+'''instruccion 2: representa las diferentes sintaxis que existen para crear comentarios en el lengiaje (una linea, varias..)'''
+#comentario en una linea
+'''esto tambien es
+un comentario en
+varias lineas'''
+
+'''instruccion 3: crea una variable (y una constante si el lengueje lo soporta)'''
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+MY_CONSTANT = "Mi constante" #por convencion
+
+'''instruccion 4: crea variables representando todos los tipos de datos primitivos del lenuaje (cadenas de texto, enteros, booleanos..)'''
+my_int = 1
+my_float = 1.5
+my_bool = "true or false"
+my_string = "mi cadena de texto"
+my_other_string = "mi otra cadena de texto"
+
+'''instruccion 5: imprime por terminal el texto ¡Hola, Python!'''
+
+'''main program below'''
+print("¡Hola, Python!")
+
+print(my_int)
+print(my_float)
+print(my_bool)
+print(my_string)
+print(my_other_string)
