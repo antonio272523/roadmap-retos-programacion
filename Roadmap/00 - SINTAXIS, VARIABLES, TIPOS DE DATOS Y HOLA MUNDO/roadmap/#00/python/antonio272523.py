c4ac559ea106f274bc104060497e2379890775ca
antonio272523 <155701035+antonio272523@users.noreply.github.com>
2024-01-06 13:56:13 -0600

    Update and rename Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ejercicio.md to Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/roadmap/#00/python/antonio272523.py
    Mi intento de resolver el reto 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/roadmap/#00/python/antonio272523.py
@@ -0,0 +1,43 @@
+# #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+> #### Dificultad: Fácil | Publicación: 26/12/23 | Corrección: 02/01/24
+
+## Ejercicio solved by antonio272523
+
+```
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ */
+```
+#https://python.org
+
+#esto es uncomentario en una linea
+'''esto tambien es
+un comentario, pero en
+varias lineas'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+MY_CONSTANT = "Mi constante" #por convencion
+
+my_int = 1
+my_float = 1.5
+my_bool = "true or false"
+my_string = "mi cadena de texto"
+my_other_string = "mi otra cadena de texto"
+
+'''main program below'''
+print("¡Hola, Python!")
+
+print(my_int)
+print(my_float)
+print(my_bool)
+print(my_string)
+print(my_other_string)
